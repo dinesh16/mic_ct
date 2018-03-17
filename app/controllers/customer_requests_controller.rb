14232ebd60611fb0fba2543318f04d717ef82792
Dinesh Anthony <anthony.dinesh16@gmail.com>
2018-03-17 16:01:55 +0000

    add customer_requests controller

--- app/controllers/customer_requests_controller.rb
+++ app/controllers/customer_requests_controller.rb
@@ -1,58 +1,24 @@
 class CustomerRequestsController < ApplicationController
-  before_action :set_customer_request, only: [:show, :edit, :update, :destroy]
-
-  # GET /customer_requests
-  def index
-    @customer_requests = CustomerRequest.all
-  end
-
-  # GET /customer_requests/1
-  def show
-  end
-
-  # GET /customer_requests/new
   def new
     @customer_request = CustomerRequest.new
   end
 
-  # GET /customer_requests/1/edit
-  def edit
-  end
-
-  # POST /customer_requests
   def create
     @customer_request = CustomerRequest.new(customer_request_params)
+    @customer_request.submit
 
-    if @customer_request.save
-      redirect_to @customer_request, notice: 'Customer request was successfully created.'
-    else
+    if @customer_request.api_errors.present?
       render :new
-    end
-  end
-
-  # PATCH/PUT /customer_requests/1
-  def update
-    if @customer_request.update(customer_request_params)
-      redirect_to @customer_request, notice: 'Customer request was successfully updated.'
     else
-      render :edit
+      redirect_to thank_you_url, notice: I18n.t(:thank_you_message)
     end
-  end
-
-  # DELETE /customer_requests/1
-  def destroy
-    @customer_request.destroy
-    redirect_to customer_requests_url, notice: 'Customer request was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_customer_request
-      @customer_request = CustomerRequest.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def customer_request_params
-      params.fetch(:customer_request, {})
-    end
+  def customer_request_params
+    params.require(:customer_request).permit(
+      :name, :business_name, :email, :telephone_number
+    )
+  end
 end
